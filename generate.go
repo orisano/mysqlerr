31df8191c7813ba4ba9b2e5e84a3387c6875efd1
Nao Yonashiro <owan.orisano@gmail.com>
2022-05-25 09:08:30 +0900

    feat: support 5.7.38 and 8.0.29

--- generate.go
+++ generate.go
@@ -1,5 +1,5 @@
 package mysqlerr
 
-//go:generate go run ./cmd/mysqlerrgen -pkg mysqlerr80 -url https://raw.githubusercontent.com/mysql/mysql-server/mysql-8.0.27/share/messages_to_clients.txt
-//go:generate go run ./cmd/mysqlerrgen -pkg mysqlerr57 -url https://raw.githubusercontent.com/mysql/mysql-server/mysql-5.7.36/sql/share/errmsg-utf8.txt
+//go:generate go run ./cmd/mysqlerrgen -pkg mysqlerr80 -url https://raw.githubusercontent.com/mysql/mysql-server/mysql-8.0.29/share/messages_to_clients.txt
+//go:generate go run ./cmd/mysqlerrgen -pkg mysqlerr57 -url https://raw.githubusercontent.com/mysql/mysql-server/mysql-5.7.38/sql/share/errmsg-utf8.txt
 
