bdc729f9fbfbf7669710d25c6f4f04a459bf1293
Nao Yonashiro <owan.orisano@gmail.com>
2025-11-06 15:30:17 +0900

    chore: update version

--- generate.go
+++ generate.go
@@ -1,6 +1,6 @@
 package mysqlerr
 
-//go:generate go run ./cmd/mysqlerrgen -pkg mysqlerr8 -url https://raw.githubusercontent.com/mysql/mysql-server/mysql-8.4.6/share/messages_to_clients.txt
-//go:generate go run ./cmd/mysqlerrgen -pkg mysqlerr80 -url https://raw.githubusercontent.com/mysql/mysql-server/mysql-8.0.43/share/messages_to_clients.txt
+//go:generate go run ./cmd/mysqlerrgen -pkg mysqlerr8 -url https://raw.githubusercontent.com/mysql/mysql-server/mysql-8.4.7/share/messages_to_clients.txt
+//go:generate go run ./cmd/mysqlerrgen -pkg mysqlerr80 -url https://raw.githubusercontent.com/mysql/mysql-server/mysql-8.0.44/share/messages_to_clients.txt
 //go:generate go run ./cmd/mysqlerrgen -pkg mysqlerr57 -url https://raw.githubusercontent.com/mysql/mysql-server/mysql-5.7.44/sql/share/errmsg-utf8.txt
 
